8f83870103a75c69181632e61867f542138374e9
AregAvetisyan82 <areg.avetisyan+1@gmail.com>
2023-06-08 19:05:04 +0400

    New files added-3.

--- pages/home_page.py
+++ pages/home_page.py
@@ -6,3 +6,8 @@ class HomePage(Helpers):
     def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
+
+    # shoes_btn = (By.XPATH, "//*[@href='/shoes']")
+    #
+    # def click_on_shoes_btn(self):
+    #     self.find_and_click(self.shoes_btn)
