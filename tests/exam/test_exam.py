019812de86e08cf6bc2767e6038564f6e6c9b6b3
AregAvetisyan82 <areg.avetisyan+1@gmail.com>
2023-06-10 15:42:54 +0400

    Last commit.

--- tests/exam/test_exam.py
+++ tests/exam/test_exam.py
@@ -8,4 +8,5 @@ class TestExam(BaseTest):
     def test_exam(self):
         self.navigate_to_free_shipping()
         self.get_text_from_page(test_data.text_to_check1)
+        self.navigate_back_to_home()
         self.search_text_and_check_tags(test_data.searching_text, test_data.text_to_check2)
